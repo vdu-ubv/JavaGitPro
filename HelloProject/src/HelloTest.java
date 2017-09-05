8c09b2d32dbfd3fa81032b7cb48d63e84880b40c
vduubv <infonet9306@gmail.com>
2017-09-05 14:23:23 +0900

    master 브런치 작업2

--- HelloProject/src/HelloTest.java
+++ HelloProject/src/HelloTest.java
@@ -4,5 +4,7 @@ public class HelloTest {
 		System.out.println("Hello world");
 		System.out.println("test 브렌치 작업 1");
 		System.out.println("test 브렌치 작업 2");
+		System.out.println("master 브런치 작업 1");
+		System.out.println("master 브런치 작업 2");
 	}
 }
