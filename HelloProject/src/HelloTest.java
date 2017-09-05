21b7d50212f6be16ae016b8d811a8b9b661b196a
vduubv <infonet9306@gmail.com>
2017-09-05 14:18:03 +0900

    test 브렌치 작업 2 추가

--- HelloProject/src/HelloTest.java
+++ HelloProject/src/HelloTest.java
@@ -3,5 +3,6 @@ public class HelloTest {
 	public static void main(String[] args){
 		System.out.println("Hello world");
 		System.out.println("test 브렌치 작업 1");
+		System.out.println("test 브렌치 작업 2");
 	}
 }
